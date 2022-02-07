be2fe82966c89c785352659309f531143e570841
Brunhilde <brunhildevink1995@gmail.com>
2022-02-07 17:59:41 +0100

    Add React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import styled from 'styled-components'
 
+import { Home } from './pages'
 import './styles/reset.css'
 
-const App = () => {
-  return <Wrapper>App</Wrapper>
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
