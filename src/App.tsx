1e1a34af74720ff1c661407b0a1d225ac02dd12b
Brunhilde <brunhildevink1995@gmail.com>
2022-02-07 17:40:30 +0100

    Add Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components'
+
 import './styles/reset.css'
 
 const App = () => {
-  return <div>App</div>
+  return <Wrapper>App</Wrapper>
 }
 
 export default App
+
+const Wrapper = styled.div`
+  height: 100%;
+`
