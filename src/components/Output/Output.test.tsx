f7ba02d61fbb882f9155c2415df48af4612bfd84
Brunhilde <brunhildevink1995@gmail.com>
2022-02-10 13:57:56 +0100

    Fix Output component test "act" warning

--- src/components/Output/Output.test.tsx
+++ src/components/Output/Output.test.tsx
@@ -1,19 +1,29 @@
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 
 import ResponseText from '.'
 import { OutputType } from '../../types'
 
 describe('ResponseText test', () => {
+  beforeAll(() => {
+    jest.useFakeTimers()
+  })
+
+  afterAll(() => {
+    jest.useRealTimers()
+  })
+
   test('ResponseText prop "text" equals "test" when given "test"', () => {
     render(<ResponseText color="white" type={OutputType.OUTPUT} text="test" />)
     const element = screen.getByText('test')
     expect(element).toBeInTheDocument()
   })
 
-  test('ResponseText should load in after 3000ms when prop is given a value of "3000"', async () => {
-    const delay = 3000
+  test('ResponseText should load in after 500ms when prop is given a value of "500"', async () => {
+    const delay = 500
     render(<ResponseText color="white" delay={delay} type={OutputType.OUTPUT} text="test" />)
-    await new Promise((r) => setTimeout(r, delay))
+    act(() => {
+      jest.advanceTimersByTime(500)
+    })
     const element = screen.getByText('test')
     expect(element).toBeInTheDocument()
   })
