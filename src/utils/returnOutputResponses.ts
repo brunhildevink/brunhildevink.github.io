65ca45f1f41816d35f4bfe3efc213a006bde15b3
Brunhilde <brunhildevink1995@gmail.com>
2022-02-21 13:31:38 +0100

    Refactor handle output

--- src/utils/returnOutputResponses.ts
+++ src/utils/returnOutputResponses.ts
@@ -15,44 +15,59 @@ const openResume = (): string => {
 const returnOutputResponses = (input: string) => {
   const inputLowerCase = input.toLowerCase()
 
-  let response: string
-  let callback = () => {}
-
-  if (inputLowerCase === 'about me') {
-    response = "My name is Brunhilde. I'm a 27 year old developer from the Netherlands."
-  } else if (inputLowerCase === 'social links') {
-    response =
-      '<a target="_blank" href="https://github.com/brunhildevink">Github</a> <a target="_blank" href="https://linkedin.com/in/brunhilde-vink">linkedIn</a>'
-  } else if (inputLowerCase.length === 0) {
-    response = ''
-  } else if (inputLowerCase === 'projects') {
-    response = 'I will display some of my projects on here in the near future. Stay tuned!'
-  } else if (inputLowerCase === 'download resume') {
-    response = 'Downloading resume...'
-    callback = () => {
-      const response = openResume()
-      console.log(response)
-    }
-  } else if (inputLowerCase.includes('hello') || inputLowerCase.includes('hi')) {
-    response = 'hello! :)'
-  } else {
-    response = `command not found: ${input}`
-  }
-
   const output: TerminalOutput[] = [
     {
       color: colors.white,
       text: input,
       type: OutputType.INPUT,
     },
-    {
-      callback: callback,
-      color: colors.white,
+  ]
+
+  if (inputLowerCase === 'about me') {
+    output.push({
+      text: "My name is Brunhilde. I'm a 27 year old developer from the Netherlands.",
+      type: OutputType.OUTPUT,
+    })
+  } else if (inputLowerCase === 'social links') {
+    output.push({
+      text: '<a target="_blank" href="https://github.com/brunhildevink">Github</a> <a target="_blank" href="https://linkedin.com/in/brunhilde-vink">linkedIn</a>',
+      type: OutputType.OUTPUT,
+    })
+  } else if (inputLowerCase.length === 0) {
+    output.push({
+      text: '',
+      type: OutputType.OUTPUT,
+    })
+  } else if (inputLowerCase === 'projects') {
+    output.push({
+      text: 'I will display some of my projects on here in the near future. Stay tuned!',
+      type: OutputType.OUTPUT,
+    })
+  } else if (inputLowerCase === 'download resume') {
+    const response = openResume()
+
+    output.push({
+      text: 'Downloading resume...',
+      type: OutputType.OUTPUT,
+    })
+
+    output.push({
+      color: colors.menuGreen,
       delay: 500,
       text: response,
       type: OutputType.OUTPUT,
-    },
-  ]
+    })
+  } else if (inputLowerCase.includes('hello') || inputLowerCase.includes('hi')) {
+    output.push({
+      text: 'hello! :)',
+      type: OutputType.OUTPUT,
+    })
+  } else {
+    output.push({
+      text: `command not found: ${input}`,
+      type: OutputType.OUTPUT,
+    })
+  }
 
   return output
 }
